eb87ff349c1ea005511909d910c115b4f4c98dd3
Naruto`kyun <fauvel.marc@free.fr>
2017-04-23 23:50:08 +0200

    Fix new height

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -593,7 +593,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {76.15 , 80.2, 1000, 144}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {76.15 , 80.2, 800, 144}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 7, 1600}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {10000,10000,10,1000000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
